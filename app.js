dd6ebb8d96d421066a7cbb9b583a90cfd875c09f
raulmanzas <raulmanzas@gmail.com>
2022-02-01 07:04:46 -0300

    adds test endpoint to validate the project setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 8080;
+
+app.get('/', (_, res) => {
+  return res.send('test endpoint');
+});
+
+app.listen(port, () => {
+  console.log(`API running on port ${port}...`);
+});
