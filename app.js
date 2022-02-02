f3f463b4e8d1909db2598d31a046dfd0917fbbbb
raulmanzas <raulmanzas@gmail.com>
2022-02-02 20:59:18 -0300

    adds logging configuration with winston

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
+const logger = require('./src/config/logger');
+
 const app = express();
 const port = 8080;
 
 app.get('/', (_, res) => res.send('test endpoint...'));
 
 app.listen(port, () => {
-  console.log(`API running on port ${port}...`);
+  logger.info(`API running on port ${port}...`);
 });
