a3373fccefa0e8a4fa1a47a3b00fb308df636388
raulmanzas <raulmanzas@gmail.com>
2022-02-02 20:29:09 -0300

    adds clean eslint configuration following airbnb style guide

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (_, res) => {
-  return res.send('test endpoint...');
-});
+app.get('/', (_, res) => res.send('test endpoint...'));
 
 app.listen(port, () => {
   console.log(`API running on port ${port}...`);
