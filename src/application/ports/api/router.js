83ee10a5ef76d7c7a7ad109d4332526bb70c0937
raulmanzas <raulmanzas@gmail.com>
2022-02-03 20:31:52 -0300

    adds default versioning on api router

--- src/application/ports/api/router.js
+++ src/application/ports/api/router.js
@@ -2,7 +2,8 @@ const express = require('express');
 const episodesController = require('./controllers/episodesController');
 
 const router = express.Router();
+const defaultVersion = 'v1';
 
-router.route('/episodes').get(episodesController.get);
+router.route(`/${defaultVersion}/episodes`).get(episodesController.get);
 
 module.exports = router;
