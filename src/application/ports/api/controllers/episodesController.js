08bb2590dfece33231ce353ec76b02e47c993183
raulmanzas <raulmanzas@gmail.com>
2022-02-03 20:34:02 -0300

    adds more samples on mocked list episodes endpoint

--- src/application/ports/api/controllers/episodesController.js
+++ src/application/ports/api/controllers/episodesController.js
@@ -4,6 +4,16 @@ function listAllEpisodes(_, res) {
       name: 'Pilot',
       season: 'S01',
       episode: 'E01'
+    },
+    {
+      name: 'Lawnmower Dog',
+      season: 'S01',
+      episode: 'E02'
+    },
+    {
+      name: 'Anatomy Park',
+      season: 'S01',
+      episode: 'E03'
     }
   ];
   return res.status(200).json(mockResponse);
