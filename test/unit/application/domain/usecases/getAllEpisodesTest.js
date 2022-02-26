f2d20e786ec5d552e384963edba3c15360cc7f23
raulmanzas <raulmanzas@gmail.com>
2022-02-26 14:02:11 -0300

    updates unit tests of getAllEpisodes

--- test/unit/application/domain/usecases/getAllEpisodesTest.js
+++ test/unit/application/domain/usecases/getAllEpisodesTest.js
@@ -3,6 +3,7 @@ const sinon = require('sinon');
 const externalClientProxy = require('../../../../../src/application/domain/proxies/externalApiProxy');
 const useCase = require('../../../../../src/application/domain/usecases/getAllEpisodes');
 const mockEpisodes = require('./mockEpisodes.json');
+const episodeHelper = require('../../../../../src/application/domain/episode');
 
 describe('get all episodes usecase', () => {
   afterEach(() => {
@@ -19,45 +20,11 @@ describe('get all episodes usecase', () => {
 
   it('when external client responds with episodes it should return them as a list', async () => {
     sinon.stub(externalClientProxy, 'getAllEpisodes').returns(mockEpisodes);
+    sinon.stub(episodeHelper, 'parseRawEpisode').returns(new Array(3));
+
     const episodes = await useCase.getAllEpisodes();
 
     assert.isArray(episodes);
     assert.strictEqual(episodes.length, 3);
-    assert.strictEqual(episodes[0].title, mockEpisodes[0].title);
-    assert.strictEqual(episodes[0].season, 'S01');
-    assert.strictEqual(episodes[0].episode, 'E01');
-  });
-
-  it('when external client respondes with episodes without titles it should throw an error', async () => {
-    const episodesWithoutTitle = mockEpisodes.map((episode) => ({
-      id: episode.id,
-      episode: episode.episode
-    }));
-    sinon.stub(externalClientProxy, 'getAllEpisodes').returns(episodesWithoutTitle);
-
-    try {
-      await useCase.getAllEpisodes();
-      assert.fail();
-    } catch (error) {
-      assert.isDefined(error);
-      assert.strictEqual(error.message, 'An episode was returned from the external API without title');
-    }
-  });
-
-  it('when external client respondes with episodes with invalid season/episode it should throw an error', async () => {
-    const episodesWithoutTitle = mockEpisodes.map((episode) => ({
-      id: episode.id,
-      title: episode.title,
-      episode: 'abcd'
-    }));
-    sinon.stub(externalClientProxy, 'getAllEpisodes').returns(episodesWithoutTitle);
-
-    try {
-      await useCase.getAllEpisodes();
-      assert.fail();
-    } catch (error) {
-      assert.isDefined(error);
-      assert.strictEqual(error.message, 'An episode was returned from the external API with an invalid season/episode');
-    }
   });
 });
